880c4633656796139c8f280336821b126b80a911
1n6ty <artyom.inety@gmail.com>
2023-10-17 00:26:22 +0700

    lab3 done

--- lab-3/main.cpp
+++ lab-3/main.cpp
@@ -6,29 +6,24 @@
 int main(){
     _setmode(_fileno(stdout), 0x00020000);
 
-
     int n;
-    float *arr = new float[n];
-    bool flag = false;
 
     std::wcout<< L"Введите натуральное чсило n\n";
     std::cin>> n;
     std::wcout<< L"Введите n действительных чисел\n";
 
+    float *arr = new float[n];
     for (int i = 0; i < n ; i++){
         std::cin>>arr[i];
     }
 
     for (int j = 0; j < n - 1; j++){
-        if(arr[j] > arr[j+1]){
-            flag = true;
-        }else{
-            flag = false;
+        if(arr[j] < arr[j+1]){
             std::wcout<< L"Последовательность не упорядочена по убыванию";
-            break;
+            return 0;
         }
     }
     
-    if(flag) std::wcout<< L"Последовательность упорядочена по убыванию";
-
+    std::wcout<< L"Последовательность упорядочена по убыванию";
+    return 0;
 }
