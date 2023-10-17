ca104c42060c8e8f58ad39b9aa9f543336cb744f
ianakaufmann <ianakau02@gmail.com>
2023-10-17 13:23:33 +0700

    индивидуальное задание для лабы2

--- lab-2/main.cpp
+++ lab-2/main.cpp
@@ -7,20 +7,28 @@ int main(){
     _setmode(_fileno(stdout), 0x00020000);
 
     int n;
-    int result1 = 1;
-    int result2 = 1;
+    long long result = 1;
 
     std::wcout << L"Введите натуральное число n\n";
     std::wcin >> n;
 
-    for (int i = 1; i <= n; i++){
-        result1 *= i;
+    if(n > 0){
+        if(n%2==0){
+            for (int i = 2; i <= n ; i += 2){
+                result *= i;
+            }
+        }else{
+             for (int i = 1; i <= n; i += 2){
+                result *= i;
+            }           
+        }
+    }else{
+        std::wcout << L"Введённое число n не является натуральным\n";
+        return 0;
     }
 
-    for (int j = 1; j <= result1; j++){
-        result2 *= j;
-    }
+    std::wcout << result;
 
-    std::wcout << result2;
+    return 0;
 
 }
