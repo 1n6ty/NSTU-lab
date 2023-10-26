c9f4c5965c4d076dd066f12167fc91e785de0ef4
ianakaufmann <ianakau02@gmail.com>
2023-10-26 10:14:50 +0700

    personal task done

--- lab-3/main.cpp
+++ lab-3/main.cpp
@@ -1,51 +1,44 @@
 #include <iostream>
+#include <algorithm>
 #include <io.h>
 #include <fcntl.h>
 #include <math.h>
-
-void sdvig(int *arr, int index, size_t N){
-    for(int i = index - 1; i < N; i++){
-        arr[i] = arr[i + 1];
-    }
-}
-
-//попробовать словарь(map)
 
 int main(){
     _setmode(_fileno(stdout), 0x00020000);
 
     int N;
-    int counter;
-    int al;
     
-    std::wcout << L"Введите натурально число N\n";
+    std::wcout << L"Введите размер массива N: ";
     std::cin >> N;
-    std::wcout << L"введите N чисел\n";
 
-    float *arr = new float[N];
-    for (int i = 0; i < N ; i++){
-        std::cin>>arr[i];
-    } 
+    int* array = new int[N];
+    std::wcout << L"Введите элементы массива:\n";
+    for (int i = 0; i < N; i++) {
+        std::cin >> array[i];
+    }
 
-    for (int j = 0; j < N; j++){
-        counter = 0;
-        for (int k = 0; k < N; k++){
-            if(arr[j] == arr[k]){
-                counter++;
+    for (int i = 0; i < N; i++) {
+        int count = 0;
+        for (int j = 0; j < N; j++) {
+            if (array[j] == array[i]) {
+                count++;
             }
         }
-        if(counter < 3){
-            int rep = arr[j];
-            for(int r = 0; r < N; r++){
-                if(rep == arr[r]){
-                    delete &arr[r];
-                }
+
+        if (count < 3) {
+            for (int j = i; j < N - 1; j++) {
+                array[j] = array[j + 1];
             }
-            al = sizeof(arr) / sizeof(arr[0]);
+            N--;
+            i--;
         }
     }
 
-    for(int d = 0; d < al; d++){
-        std::cin >> arr[d];
+    std::wcout << L"Размер полученного массива: " << N << std::endl;
+    std::wcout << L"Содержимое полученного массива:";
+    for (int i = 0; i < N; i++) {
+        std::wcout << array[i] << " ";
     }
+    return 0;
 }
