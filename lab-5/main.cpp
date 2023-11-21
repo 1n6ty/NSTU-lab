43e0d26d12859bec1b8196a9ba73d0b03b35d088
ianakaufmann <ianakau02@gmail.com>
2023-11-21 08:38:05 +0700

    main lab-5 done

---
+++ lab-5/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <math.h>
+
+int sym(int x[], int y[]){
+    int summa = 0;
+    for (int k = 0; k < 20; k++){
+        summa += x[k]*y[k];
+    }
+    return summa;
+}
+
+int main(){
+    _setmode(_fileno(stdout), 0x00020000);
+
+    int x[20];
+    int y[20];
+    int s1 = 0;
+
+
+    std::wcout << L"Введите 20 целых чисел для массива х. \n";
+    for (int i = 0; i < 20; i++){
+        std::cin >> x[i];
+    }
+
+    std::wcout << L"Введите 20 целых чисел для массива y. \n";
+    for (int j = 0; j < 20; j++){
+        std::cin >> y[j];
+    }
+
+    if(sym > 0){
+        for (int r = 0; r < 20; r++){
+            s1 += x[r] * x[r];
+        }
+    }else{
+        for (int t = 0; t < 20; t++){
+            s1 += y[t] * y[t];
+        }
+    }
+
+    std::wcout << s1;
+    
+    return 0;
+}
