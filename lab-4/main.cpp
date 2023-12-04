0a97cbbe2120e96dc047a4cf18376c7477a5d073
ianakaufmann <ianakau02@gmail.com>
2023-12-04 14:49:07 +0700

    main in lb-4 done

--- lab-4/main.cpp
+++ lab-4/main.cpp
@@ -7,19 +7,18 @@
 const int MAX_LENGTH = 40;
 
 int main(){
-    _setmode(_fileno(stdout), 0x00020000);
 
     std::string str;
     char d;
     int length = 0;
 
-    std::wcout << L"Введите последовательность не более чем из 40 строчных латинских букв, за которой следуют пробелы. \n";
+    std::cout << "Enter a sequence of no more than 40 lowercase Latin letters followed by spaces. \n";
 
     while (length < MAX_LENGTH && std::cin.get(d) && d != '\n') {
         if (d >= 'a' && d <= 'z') {
             str += d;
         } else {
-            std::wcout << L"Введены некорректные символы. \n";
+            std::cout << "Incorrect characters have been entered. \n";
             return 0;
         }
     }
@@ -28,7 +27,7 @@ int main(){
         str.replace(str.find("abc"), 3, "def");
     }
     
-    std::cin >> str;
+    std::cout << str;
 
     return 0;
 }
