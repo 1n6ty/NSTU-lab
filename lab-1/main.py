37509874f2abd3752900771c3bb229290ebabffb
1n6ty <artyom.inety@gmail.com>
2024-02-28 12:21:15 +0700

    add comments

--- lab-1/main.py
+++ lab-1/main.py
@@ -1,14 +1,14 @@
 s = [*input()]
 
-left_ind, right_ind = 0, len(s) - 1
+left_ind, right_ind = 0, len(s) - 1 # lefthand and righthand indexes
 
-while(left_ind < right_ind):
-    if s[left_ind] == 'b' and s[right_ind] == 'a':
+while(left_ind < right_ind): # Check if indexes are crossed
+    if s[left_ind] == 'b' and s[right_ind] == 'a': # Swap symbols if condition is met
         s[left_ind], s[right_ind] = s[right_ind], s[left_ind]
 
-    if s[left_ind] != 'b':
+    if s[left_ind] != 'b': # Search for 'b' from left
         left_ind += 1
-    if s[right_ind] != 'a':
+    if s[right_ind] != 'a': # Search for 'a' from right
         right_ind -= 1
 
 print(''.join(s))
