243b1ca6bea2f920208218f8031a6cf1beb2f7d1
ianakaufmann <ianakau02@gmail.com>
2024-03-19 12:37:37 +0700

    report lab-2 not done

--- lab-2/main_1g.cpp
+++ lab-2/main_1g.cpp
@@ -13,6 +13,13 @@ struct Queue { //структура очереди
 };
 
 void initQueue(Queue& q, int capacity) { //создание очереди нулевой длины/очистка очереди
+    // Node* current = q.front;
+    // while (current != nullptr) {
+    //     Node* temp = current;
+    //     current = current->next;
+    //     delete temp;
+    // }
+
     q.front = nullptr;
     q.rear = nullptr;
     q.size = 0;
@@ -45,7 +52,7 @@ void printQueue(Queue q) { //вывод очереди
 
 int enqueue(Queue& q, int value) { //добавление элемента в очередь
     if (isFull(q)) {
-        return 1; // Очередь переполнена
+        return 1; //очередь переполнена
     }
 
     Node* newNode = new Node;
@@ -68,7 +75,7 @@ int enqueue(Queue& q, int value) { //добавление элемента в о
 
 int dequeue(Queue& q, int& value) { //удаление элемента из очереди
     if (isEmpty(q)) {
-        return 2; // Очередь пуста
+        return 2; //очередь пуста
     }
 
     Node* temp = q.front;
@@ -99,44 +106,45 @@ int main() {
 
     while (end){
 
-        std::cout << "To clear the queue, enter 1.\n" <<
-                     "To check whether the queue is empty, press 2.\n" <<
-                     "To add an item to the queue, press 3.\n" << 
-                     "To remove an item from the queue, enter 4. \n";
+        std::cout << "To clear the queue, enter 1.\n" << //чтобы очистить очередь, введите 1
+                     "To check whether the queue is empty, press 2.\n" << //чтобы проверить является ли очередь пустой, введите 2
+                     "To add an item to the queue, press 3.\n" << //чтобы добавить элемент в очередь, нажмите 3
+                     "To remove an item from the queue, enter 4. \n"; //чтобы удалить элемент из очереди, введите 4
         std::cin >> choice;
 
         if(choice == 1){
             initQueue(q, capacity);
-            std::cout << "Queue is empty!\n";
+            std::cout << "Queue is empty!\n"; //очередь пуста
         }else if (choice == 2){
             if (isEmpty(q)) {
-                std::cout << "Queue is empty!\n";
+                std::cout << "Queue is empty!\n"; //очередь пуста
             } else {
-                std::cout << "Queue is not empty.\n";
+                std::cout << "Queue is not empty.\n"; //очередь не пуста
             }
         }else if (choice == 3){
-            std::cout << "Enter element for add: ";
+            std::cout << "Enter element for add: "; //введите элемент для добавления
             std::cin >> element;
             result = enqueue(q, element);
             if (result == 1) {
-                std::cout << "Queue is full!\n";
+                std::cout << "Queue is full!\n"; //очередь переполнена
             } else {
-                std::cout << "Element in queue.\n";
+                std::cout << "Element in queue.\n"; //элемент добавлен в очередь
             }
         }else if (choice == 4){
             int extractedElement;
             result = dequeue(q, extractedElement);
             if (result == 2) {
-                std::cout << "Queue is empty!\n";
+                std::cout << "Queue is empty!\n"; //очередь пуста
             } else {
-                std::cout << "Takes element: " << extractedElement << "\n";
+                std::cout << "Takes element: " << extractedElement << "\n"; //элемент для удаления
             }
         }else{
-            std::cout << "Incorect value.\n";
+            std::cout << "Incorect value.\n"; //некорректное значение
             end = false;
+            break;
         }
 
-        std::cout << "Enter 1 if you want to continue working with the queue and 0 if you don't want to. \n";
+        std::cout << "Enter 1 if you want to continue working with the queue and 0 if you don't want to. \n"; //введите 1, если хотите продолжить работу с очередью, и 0, если хотите закончить
         std::cin >> exit;
         if (exit == 0) end = false;
     }
