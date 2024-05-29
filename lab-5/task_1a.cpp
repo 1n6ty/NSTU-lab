11b975d5b2f1b2559403a0f8c593c92215dbb11b
ianakaufmann <ianakau02@gmail.com>
2024-05-29 11:59:06 +0700

    task_1a not done

--- lab-5/task_1a.cpp
+++ lab-5/task_1a.cpp
@@ -7,6 +7,7 @@ struct Element {
 };
 
 void insertionSort(Element arr[], int n, int& comparisons, int& moves) {
+    
     for (int i = 1; i < n; ++i) {
         Element temp = arr[i];
         int j = i - 1;
@@ -31,19 +32,29 @@ int main() {
 
     Element table[n];
 
-    std::cout << "Enter elements (key followed by value):\n";
+
     for (int i = 0; i < n; ++i) {
-        std::cin >> table[i].key >> table[i].value;
+        std::cout << "Enter element key(no more than 15 characters): ";
+        std::cin >> table[i].key;
+        if(table[i].key.size() > 15){
+            std::cout << "\nEnter invalid key.\n";
+            return 0;
+        }
+        std::cout << "Enter element: ";
+        std::cin >> table[i].value;
     }
 
     int comparisons = 0;
     int moves = 0;
     insertionSort(table, n, comparisons, moves);
 
-    std::cout << "Sorted table:\n";
+    std::cout << "\nSorted table:\n";
     for (int i = 0; i < n; ++i) {
         std::cout << table[i].key << " " << table[i].value << std::endl;
     }
+
+    std::cout << "Number of comparisons: " << comparisons << std::endl;
+    std::cout << "Number of moves: " << moves << std::endl;
 
     return 0;
 }
