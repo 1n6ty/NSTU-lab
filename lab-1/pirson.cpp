a936cb56fca700f8617ef59409bb62ea8c8e0a72
1n6ty <artyom.inety@gmail.com>
2025-09-27 12:46:25 +0700

    update architecture

--- lab-1/pirson.cpp
+++ lab-1/pirson.cpp
@@ -1,25 +1,25 @@
 /**
- * @file pirson.cpp
- * @author Artyom Eroshchenko <eroshhenko.2023@stud.nstu.ru>
+ * @file    pirson.cpp
+ * @author  Artyom Eroshchenko <eroshhenko.2023@stud.nstu.ru>
  * @version 1.0
  *
  * @section DESCRIPTION
  *
- * This file initializes structures and functions for Pirson-VII 
- * distribution. For more see @ref pirson.h "Pirson-VII header file".
+ *          This file initializes structures and functions for Pirson-VII 
+ *          distribution. For more see @ref pirson.h "Pirson-VII header file".
  */
 
 #include "pirson.h"
 
 /**
- * @author Artyom Eroshchenko <eroshhenko.2023@stud.nstu.ru>
+ * @author  Artyom Eroshchenko <eroshhenko.2023@stud.nstu.ru>
  * @version 1.0
  * 
- * @brief Computes density of pirson-VII distribution.
+ * @brief   Computes density of pirson-VII distribution.
  * 
- * @param x point at which the density will be computed
- * @return probability density at @p x point
+ * @param   x point at which the density will be computed
+ * @return  probability density at @p x point
  */
-double Pirson_VII::compute_density(double x){
+double Pirson_VII::compute_density(double x, struct Pirson_p *p){
     // Realization
 }
--- lab-1/pirson.h
+++ lab-1/pirson.h
@@ -1,30 +1,47 @@
 /**
- * @file pirson.h
- * @author Artyom Eroshchenko <eroshhenko.2023@stud.nstu.ru>
+ * @file    pirson.h
+ * @author  Artyom Eroshchenko <eroshhenko.2023@stud.nstu.ru>
  * @version 1.0
  *
  * @section DESCRIPTION
  *
- * Header file for Pirson-VII distribution structures and functions.
+ *          Header file for Pirson-VII distribution structures and functions.
  */
 
 #ifndef PIRSON_H
 #define PIRSON_H
 
+#include <string>
+
 /**
- * @struct Pirson_VII
- * @version 1
+ * @struct  Pirson_VII
+ * @author  Artyom Eroshchenko <eroshhenko.2023@stud.nstu.ru>
+ * @version 1.0
  * 
- * @brief The structure for Pirson-VII distribution.
+ * @brief   The structure for Pirson-VII distribution.
  * @details This Pirson-VII structure provides functions
  *          to compute density, mathematical expectation, dispersion,
  *          skewness and excess coefficient for pirson-VII distribution.
- * 
- * @author Artyom Eroshchenko
+ *          Also function to generate random variable under this distribution.
  */
 struct Pirson_VII{
-    double compute_density(double x);
-    double compute_mat_expectation(double x);
+    double compute_density(double x, struct Pirson_p *p);
+    double generate_x(struct Pirson_p *p);
+    double compute_mat_expectation(struct Pirson_p *p);
+    double compute_dispersion(struct Pirson_p *p);
+    double compute_skewness(struct Pirson_p *p);
+    double compute_excess(struct Pirson_p *p);
+};
+
+/**
+ * @struct  Pirson_VII
+ * @author  Artyom Eroshchenko <eroshhenko.2023@stud.nstu.ru>
+ * @version 1.0
+ * 
+ * @brief   The structure of parameters for Pirson-VII distribution.
+ */
+struct Pirson_p{
+    double v;
 };
 
 #endif
