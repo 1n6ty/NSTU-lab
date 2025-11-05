01ff8d15e4fd37d14857f975e3f22551c8583636
Artyom Eroshchenko <artyom.inety@gmail.com>
2025-11-05 16:42:28 +0700

    fix updating constant and add comments

--- lab-2/pearson/pearson.cpp
+++ lab-2/pearson/pearson.cpp
@@ -12,6 +12,17 @@
 #include <cmath>
 #include <random>
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Constructor of pearson-VII distribution.
+ * 
+ * @details Throws PearsonException if invalid parameters.
+ * 
+ * @param   shift shift parameter
+ * @param   scale scale parameter
+ * @param   shape shape parameter
+ */
 PearsonVII::PearsonVII(double shift, double scale, double shape){
     if (shape <= 0.5 || scale <= 0) throw PearsonException("Invalid parameters for pearson distribution (shape > 0.5 and scale > 0).");
 
@@ -22,6 +33,15 @@ PearsonVII::PearsonVII(double shift, double scale, double shape){
     updateNormalizationConstant();
 }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Constructor of pearson-VII distribution (load from file).
+ * 
+ * @details Throws PearsonException if invalid parameters or file errors.
+ * 
+ * @param   filename name of file to load from
+ */
 PearsonVII::PearsonVII(const std::string& filename){
     std::ifstream file(filename);
     if (!file.is_open()) {
@@ -42,22 +62,51 @@ PearsonVII::PearsonVII(const std::string& filename){
     this->scale = scale;
     this->shape = shape;
 
+    updateNormalizationConstant();
+
     file.close();
 }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Method to update normalization constant.
+ */
 inline void PearsonVII::updateNormalizationConstant(){
     this->normalization_constant = 1.0 / (scale * std::beta(this->shape - 0.5, 0.5) * std::pow(1.0, this->shape - 0.5));
 }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Getters.
+ */
 double PearsonVII::getShift() const { return this->shift; }
 double PearsonVII::getScale() const { return this->scale; }
 double PearsonVII::getShape() const { return this->shape; }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Computes density of pearson-VII distribution.
+ * 
+ * @param   x point at which the density will be computed
+ * @return  probability density at @p x point
+ */
 double PearsonVII::computeDensity(double x) const {
     double z = (x - this->shift) / this->scale;
     return this->normalization_constant * std::pow(1.0 + z * z, -this->shape);
 }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Computes Mathematical Expectation of pearson-VII distribution.
+ * 
+ * @details If `shape` is less or equal `1` then throws PearsonException
+ * 
+ * @return  Mathematical Expectation
+ */
 double PearsonVII::computeExpectation() const {
     if (this->shape <= 1.0){
         throw PearsonException("Expectation does not exist for shape <= 1");
@@ -65,6 +114,15 @@ double PearsonVII::computeExpectation() const {
     return this->shift;
 }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Computes Variance of pearson-VII distribution.
+ * 
+ * @details If `shape` is less or equal `1.5` then throws PearsonException
+ * 
+ * @return  Dispersion
+ */
 double PearsonVII::computeVariance() const {
     if (this->shape <= 1.5){
         throw PearsonException("Variance does not exist for shape <= 1.5");
@@ -72,6 +130,15 @@ double PearsonVII::computeVariance() const {
     return this->scale * this->scale / (2.0 * this->shape - 3.0);
 }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Computes Skewness of pearson-VII distribution.
+ * 
+ * @details If `shape` is less or equal `1.5` then throws PearsonException
+ * 
+ * @return  Skewness
+ */
 double PearsonVII::computeSkewness() const {
     if (this->shape <= 1.5){
         throw PearsonException("Skewness does not exist for shape <= 1.5");
@@ -79,6 +146,15 @@ double PearsonVII::computeSkewness() const {
     return 0.0;
 }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Computes Kurtosis of pearson-VII distribution.
+ * 
+ * @details If `shape` is less or equal `2.0` then throws PearsonException
+ * 
+ * @return  Kurtosis
+ */
 double PearsonVII::computeKurtosis() const {
     if (this->shape <= 2.0){
         throw PearsonException("Kurtosis does not exist for shape <= 2");
@@ -86,6 +162,13 @@ double PearsonVII::computeKurtosis() const {
     return 6.0 / (2.0 * this->shape - 5.0);
 }
 
+/**
+ * @version 1.0
+ * 
+ * @brief   Generates value out of pearson-VII distribution.
+ * 
+ * @return  generated value
+ */
 double PearsonVII::generateRandom() const {
     static std::random_device rd;
     static std::mt19937 gen(rd());
