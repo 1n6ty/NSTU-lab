21cd3bac0f305375c729305150a1e7eec5bc6944
Artyom Eroshchenko <artyom.inety@gmail.com>
2026-09-22 12:06:08 +0700

    partial work done

--- lab-1/main.py
+++ lab-1/main.py
@@ -28,6 +28,8 @@ def symmetric_uncertainty_sklearn(x, y):
     mi = mutual_info_score(x_clean, y_clean) / np.log(2)
     return 2.0 * mi / (h_x + h_y)
 
+REMOVE_K = 50
+
 BASE_DIR = Path(__file__).resolve().parent
 
 DATA_HOME_CREDIT_PATH = BASE_DIR / "DataHomeCredit.csv"
@@ -36,6 +38,7 @@ DESCRIPTION_HOME_CREDIT = BASE_DIR / "DescriptionHomeCredit.csv"
 desc_df = pd.read_csv(DESCRIPTION_HOME_CREDIT, sep=";", encoding='unicode_escape', comment="#")
 
 df = pd.read_csv(DATA_HOME_CREDIT_PATH, sep=",", encoding='unicode_escape', comment="#")[list(desc_df['Column'])]
+initial_df = df
 
 def clear_k_least_freq(df: pd.DataFrame, col_name: str, k = 1):
     counts = df[col_name].value_counts(dropna=True)
@@ -47,59 +50,58 @@ for col in df.columns:
     print(df[col].value_counts(normalize=True, dropna=False) * 100)
     print("\n")
 
-print("\n\n-------(k = 0)-------\n\n")
-for i in list(desc_df['Column']):
-    if i != "TARGET": 
-        print(f"{i} vs TARGET: ", symmetric_uncertainty_sklearn(df[i], df['TARGET']))
-        
-print("\n\n-------(k = 1)-------\n\n")
-for i in list(desc_df['Column']):
-    filtered = clear_k_least_freq(df, i, 1)
-    if i != "TARGET": 
-        print(f"{i} vs TARGET: ", symmetric_uncertainty_sklearn(filtered[i], filtered['TARGET']))
+LL_COLUMNS = ['OCCUPATION_TYPE', 'ORGANIZATION_TYPE']
+TARGET_COLUMN = 'TARGET'
 
-print("\n\n-------(k = 2)-------\n\n")
-for i in list(desc_df['Column']):
-    filtered = clear_k_least_freq(df, i, 2)
+print(f"\nFiltering only large-level variables: {LL_COLUMNS}")
+
+df = df[LL_COLUMNS + [TARGET_COLUMN]]
+
+print("\n\n-------(SU before removing)-------\n")
+for i in LL_COLUMNS:
+    if i != "TARGET":
+        print(f"{i} vs TARGET: ", symmetric_uncertainty_sklearn(df[i], df['TARGET']))
+
+print(f"\n\n-------(SU after removing {REMOVE_K} layers with lowest frequences)-------\n")
+for i in LL_COLUMNS:
+    filtered = clear_k_least_freq(df, i, REMOVE_K)
     if i != "TARGET": 
         print(f"{i} vs TARGET: ", symmetric_uncertainty_sklearn(filtered[i], filtered['TARGET']))
         
-        
+print(f"\n\n-------(SU diff)-------\n")
+for i in LL_COLUMNS:
+    filtered = clear_k_least_freq(df, i, REMOVE_K)
+    if i != "TARGET": 
+        print(f"{i} vs TARGET: ", symmetric_uncertainty_sklearn(filtered[i], filtered['TARGET']) - symmetric_uncertainty_sklearn(df[i], df['TARGET']))
+
+print("\n\n--------Statistical hypothesis testing---------\n")
+
 from scipy.stats import chi2_contingency, fisher_exact
 
 def test_rare_category_significance(df, col_name, k=2):
-    # 1. Находим k самых редких категорий
     counts = df[col_name].value_counts(dropna=True)
     rare_cats = counts.nsmallest(k).index
     
-    # 2. Делим выборку на "редкие категории" и "остальные"
     is_rare = df[col_name].isin(rare_cats)
     
-    # Строим таблицу сопряженности 2x2: [Rare/Not Rare] x [TARGET 0/1]
     contingency_table = pd.crosstab(is_rare, df['TARGET'])
     contingency_table.index = ['Other Categories', 'Rare Categories']
     
-    print(f"=== Проверка для колонки: {col_name} ===")
-    print("Таблица сопряженности:")
+    print(f"-------Test for column: {col_name}--------")
+    print("Contingency Table:")
     print(contingency_table)
-    print("\nДоля TARGET=1:")
+    print("\nPart of TARGET=1:")
     print(df.groupby(is_rare)['TARGET'].mean().rename({False: 'Other Categories', True: 'Rare Categories'}))
     
-    # 3. Выполняем тест (Chi-square или Fisher)
-    # Если объемы в редких категориях совсем малы (< 5), лучше использовать exact Fisher test
-    if contingency_table.values.min() < 5:
-        res = fisher_exact(contingency_table)
-        p_val = res.pvalue
-        test_name = "Fisher's Exact Test"
-    else:
-        chi2, p_val, dof, ex = chi2_contingency(contingency_table)
-        test_name = "Chi-Square Test"
+    res = fisher_exact(contingency_table)
+    p_val = res.pvalue
         
-    print(f"\n{test_name} p-value: {p_val:.5f}")
+    print(f"\nFisher's Exact Test p-value: {p_val:.5f}")
     if p_val < 0.05:
-        print("Результат: Отличие СТАТИСТИЧЕСКИ ЗНАЧИМО. Редкие категории содержали важный сигнал!")
+        print("Result: Low-freq categories contained necessary signal.")
     else:
-        print("Результат: Отличие НЕЗНАЧИТЕЛЬНО. Редкие категории были шумом.")
+        print("Result: Low-freq categories are noise.")
     print("-" * 50)
     
-print(test_rare_category_significance(df, 'ORGANIZATION_TYPE', k=40))
+for i in LL_COLUMNS:
+    test_rare_category_significance(initial_df, i, k=REMOVE_K)
